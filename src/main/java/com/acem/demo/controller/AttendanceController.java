f665149a51bc1f88ea2167261eec22b27ef73aef
mnzit <mnzitshakya@gmail.com>
2022-11-26 12:26:48 +0545

    typos and wrong imports

--- src/main/java/com/acem/demo/controller/AttendanceController.java
+++ src/main/java/com/acem/demo/controller/AttendanceController.java
@@ -1,7 +1,7 @@
 package com.acem.demo.controller;
 
-import com.acem.demo.model.Student;
-import com.acem.demo.repository.attendanceRepository;
+import com.acem.demo.model.Attendance;
+import com.acem.demo.repository.AttendanceRepository;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +16,11 @@ public class AttendanceController {
     private final AttendanceRepository attendanceRepository;
 
     public AttendanceController(AttendanceRepository attendanceRepository) {
-        this.AttendanceRepository = attendanceRepository;
+        this.attendanceRepository = attendanceRepository;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Student> students() {
+    public List<Attendance> students() {
         return attendanceRepository.findAll();
     }
 }
--- src/main/java/com/acem/demo/repository/AttendanceRepository.java
+++ src/main/java/com/acem/demo/repository/AttendanceRepository.java
@@ -1,5 +1,6 @@
 package com.acem.demo.repository;
 
+import com.acem.demo.model.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

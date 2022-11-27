b7dee322e1a005ce8c0ac1eae692a8bdba5e0737
bishnukdk <bishnukhadka442@gmail.com>
2022-11-27 22:19:42 +0545

    retrieve and delete feature of Subject repository added

--- src/main/java/com/acem/demo/constant/ResponseMessageConstant.java
+++ src/main/java/com/acem/demo/constant/ResponseMessageConstant.java
@@ -10,13 +10,24 @@ public interface ResponseMessageConstant {
         String UPDATED = "Student updated successfully";
         String NOT_SAVED = "Student not saved";
         String NOT_UPDATED = "Student not updated";
-
         String NOT_DELETED = "Student not deleted";
-
         String DELETED = "Student deleted successfully";
     }
 
     String SERVER_ERROR = "Server Error";
     String INVALID_PATH_PARAMETER = "Not a valid request";
     String INVALID_REQUEST_BODY = "Not a valid request";
+
+    interface Subject {
+        String ONE = "Subject fetched successfully";
+        String ALL = "Subjects fetched successfully";
+        String NOT_FOUND = "Subjects not found";
+        String SAVED = "Subject saved successfully";
+        String UPDATED = "Subject updated successfully";
+        String NOT_SAVED = "Subject not saved";
+        String NOT_UPDATED = "Subject not updated";
+        String NOT_DELETED = "Subject not deleted";
+        String DELETED = "Subject deleted successfully";
+    }
+
 }
---
+++ src/main/java/com/acem/demo/controller/SubjectController.java
@@ -0,0 +1,36 @@
+package com.acem.demo.controller;
+
+import com.acem.demo.model.Student;
+import com.acem.demo.model.Subject;
+import com.acem.demo.response.Response;
+import com.acem.demo.service.SubjectService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/subjects")
+public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response subjects() {
+        return subjectService.getAll();
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response subject(@PathVariable("id") Long id) {
+        return subjectService.getById(id);
+    }
+
+    @DeleteMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response delete(@PathVariable("id") Long id){
+        return subjectService.delete(id);
+    }
+
+}
